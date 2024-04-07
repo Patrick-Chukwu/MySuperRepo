1b04263a4bed0c7f84c79a5e99f4d15d382165bc
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-04-07 05:46:02 +0100

    Introduce routing

--- react_projects/gpt-3/src/App.js
+++ react_projects/gpt-3/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Routes, Route, Link} from "react-router-dom";
 
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
 import { CTA, Brand, Navbar, Article, Feature } from './components';
@@ -6,16 +7,26 @@ import './App.css';
 
 const App = () => {
     return (
+        <div>
+            <Link to="/features">Features</Link>
+
+        <Routes>
+            <Route path="/" element={<Header />}></Route>
+            <Route path="/brand" element={<Brand />}></Route>
+            <Route path="/whatgpt3" element={ <WhatGPT3 />}></Route>
+            <Route path="/possibility" element={<Possibility />}></Route>
+            <Route path="/features" element={<Features />}></Route>
+        </Routes>
         <div className="App">
             <div className="gradient__bg">
-                <Navbar />
-                <Header />
+                {/* <Navbar />
+                <Header /> */}
 
             </div> 
-            <Brand />
-            <WhatGPT3 />
+            
+           
             <Features />
-            <Possibility />
+            {/* <Possibility /> */}
             <CTA />
             <Blog />
             <Article />
@@ -24,6 +35,8 @@ const App = () => {
 
        
         </div>
+        </div>
+
     )
 }
 
--- react_projects/gpt-3/src/index.js
+++ react_projects/gpt-3/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+    <BrowserRouter><App /></BrowserRouter>
+)
